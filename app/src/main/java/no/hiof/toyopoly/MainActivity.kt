81ab47f9f44adfec2bb62ec35cce4c025125fd15
Mujibullah.R <mujira00@gmail.com>
2022-09-06 15:33:02 +0200

    updated MainActivity-layout, added LegoActivity w/clickListener

--- app/src/main/java/no/hiof/toyopoly/MainActivity.kt
+++ app/src/main/java/no/hiof/toyopoly/MainActivity.kt
@@ -1,11 +1,21 @@
 package no.hiof.toyopoly
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val legoActivityBtn = findViewById<Button>(R.id.legoBtn)
+
+        legoActivityBtn.setOnClickListener(View.OnClickListener {
+            val legoActivityIntent = Intent(this, LegoActivity::class.java)
+            startActivity(legoActivityIntent)
+        })
     }
 }
