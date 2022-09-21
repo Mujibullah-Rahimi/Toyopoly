cd167af4eaf751a8575f9226827cf5b231b45611
Mujibullah.R <mujira00@gmail.com>
2022-09-21 10:27:37 +0200

    feat: user sign in to application with firebaseAuth working

---
+++ app/src/main/java/no/hiof/toyopoly/LoginFragment.kt
@@ -0,0 +1,73 @@
+package no.hiof.toyopoly
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.navigation.findNavController
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.app
+
+class LoginFragment : Fragment(), View.OnClickListener {
+    private lateinit var auth: FirebaseAuth
+
+    // Initializing a FirebaseAuth instance
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val loginButton = view.findViewById<Button>(R.id.loginButton)
+        loginButton.setOnClickListener(this)
+
+        val registerButton = view.findViewById<Button>(R.id.registerButton)
+        registerButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        auth = FirebaseAuth.getInstance()
+        val navController = v?.findNavController()
+        val emailField = requireView().findViewById<EditText>(R.id.emailAdressLogin)
+        val passwordField = requireView().findViewById<EditText>(R.id.passwordLogin)
+
+        val email: String= emailField?.text.toString()
+        val password: String = passwordField?.text.toString()
+
+        when (v?.id) {
+            R.id.loginButton -> {
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(requireActivity(), OnCompleteListener { task ->
+                    if(task.isSuccessful) {
+                        Toast.makeText(activity, "Successfully Logged In", Toast.LENGTH_LONG).show()
+                        val action = LoginFragmentDirections.actionLoginFragmentToHomeFragment()
+
+                        navController?.navigate(action)
+                    }else {
+                        Toast.makeText(activity, "Login Failed", Toast.LENGTH_LONG).show()
+                    }
+                })
+            }
+            R.id.registerButton ->{
+                val action = LoginFragmentDirections.actionLoginFragmentToRegisterFragment()
+                navController?.navigate(action)
+            }
+        }
+    }
+}
