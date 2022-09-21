f8c03255aeb9d719eec0f5569b3339f748c608c9
Mujibullah.R <mujira00@gmail.com>
2022-09-21 09:45:54 +0200

    feat: firebaseAuth set up so users registered are created in firebase

---
+++ app/src/main/java/no/hiof/toyopoly/RegisterFragment.kt
@@ -0,0 +1,63 @@
+package no.hiof.toyopoly
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import android.content.Intent
+import android.text.TextUtils
+import com.google.android.gms.tasks.OnCompleteListener
+
+class RegisterFragment : Fragment() {
+    private lateinit var auth:FirebaseAuth
+    private lateinit var registerEmail: EditText
+    private lateinit var registerPassword: EditText
+    private lateinit var registerButton: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+
+        registerEmail  = requireView().findViewById(R.id.emailAdressRegister)
+        registerPassword  = requireView().findViewById(R.id.passwordRegister)
+        registerButton = requireView().findViewById(R.id.UserRegisterButton)
+
+        registerButton.setOnClickListener{
+            val email: String = registerEmail.text.toString()
+            val password: String = registerPassword.text.toString()
+
+            if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                Toast.makeText(activity, "Please fill all the fields", Toast.LENGTH_LONG).show()
+            } else {
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener((requireActivity()), OnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(activity, "Successfully Registered", Toast.LENGTH_LONG)
+                                .show()
+                            val intent = Intent(activity, MainActivity::class.java)
+                            startActivity(intent)
+                            activity?.finish()
+                        } else {
+                            Toast.makeText(activity, "Registration Failed", Toast.LENGTH_LONG)
+                                .show()
+                        }
+                    })
+                }
+        }
+    }
+}
