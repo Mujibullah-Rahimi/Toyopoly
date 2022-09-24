c65470af4253c81c495f3e2faa0d5799694b4273
Mujibullah.R <mujira00@gmail.com>
2022-09-24 09:47:43 +0200

    feat: registration takes in date in dd/mm/yyyy format, and all fields are required to be filled out

--- app/src/main/java/no/hiof/toyopoly/RegisterFragment.kt
+++ app/src/main/java/no/hiof/toyopoly/RegisterFragment.kt
@@ -1,7 +1,8 @@
 package no.hiof.toyopoly
 
-import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.NoCopySpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,15 +11,15 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
-import android.content.Intent
 import android.text.TextUtils
+import android.text.TextWatcher
 import androidx.navigation.fragment.findNavController
 import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import no.hiof.toyopoly.userManagment.DateInputMask
 
-class RegisterFragment : Fragment() {
+class RegisterFragment : Fragment(), NoCopySpan{
     private lateinit var auth:FirebaseAuth
     private lateinit var registerEmail: EditText
     private lateinit var registerPassword: EditText
@@ -38,7 +39,7 @@ class RegisterFragment : Fragment() {
         auth = FirebaseAuth.getInstance()
         val db = Firebase.firestore
 
-        registerEmail  = requireView().findViewById(R.id.emailAdressRegister)
+        registerEmail  = requireView().findViewById(R.id.emailAddressRegister)
         registerPassword  = requireView().findViewById(R.id.passwordRegister)
         registerButton = requireView().findViewById(R.id.UserRegisterButton)
 
@@ -47,15 +48,32 @@ class RegisterFragment : Fragment() {
         val registerBirthday = requireView().findViewById<EditText>(R.id.birthday)
         val registerAddress = requireView().findViewById<EditText>(R.id.address)
 
+        DateInputMask(registerBirthday).listen()
+
         registerButton.setOnClickListener{
             val email: String = registerEmail.text.toString()
             val password: String = registerPassword.text.toString()
+            val birthday: String = registerBirthday.text.toString()
+            val address: String = registerAddress.text.toString()
+            val firstName: String = registerFirstName.text.toString()
+            val lastName: String = registerLastName.text.toString()
             val navController = findNavController()
             val action = RegisterFragmentDirections.actionRegisterFragmentToLoginFragment()
 
-            if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            if(
+                TextUtils.isEmpty(email) ||
+                TextUtils.isEmpty(password) ||
+                TextUtils.isEmpty(birthday) ||
+                TextUtils.isEmpty(address) ||
+                TextUtils.isEmpty(firstName) ||
+                TextUtils.isEmpty(lastName)) {
                 Toast.makeText(activity, "Please fill all the fields", Toast.LENGTH_LONG).show()
-            } else {
+            }
+            else if (birthday.length != 10){
+                Toast.makeText(activity, "Date of birth is not valid", Toast.LENGTH_LONG).show()
+
+            }
+            else {
                 auth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener((requireActivity()), OnCompleteListener { task ->
                         if (task.isSuccessful) {
@@ -72,7 +90,7 @@ class RegisterFragment : Fragment() {
                                 .show()
                             navController.navigate(action)
                         } else {
-                            Toast.makeText(activity, "Registration Failed", Toast.LENGTH_LONG)
+                            Toast.makeText(activity, task.exception?.message, Toast.LENGTH_LONG)
                                 .show()
                         }
                     })
