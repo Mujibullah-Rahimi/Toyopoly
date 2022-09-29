460ad84ce64bc6bd08d1337fdfbb87c95a9be65c
Herman Kristiansen <herman.kristiansen21@gmail.com>
2022-09-29 17:22:51 +0200

    Feat: halvveis ferdig mulighet for å hente alle ads ved kategori
    denne løsningen er ikke ferdig og er fremdeles ganske ødelagt

---
+++ app/src/main/java/no/hiof/toyopoly/Ads.kt
@@ -0,0 +1,5 @@
+package no.hiof.toyopoly
+
+import java.security.Timestamp
+
+data class Ads(var Vare: String, var Desc: String, var Price: Long, var TimeStamp: Timestamp, var Category: String)
--- app/src/main/java/no/hiof/toyopoly/CategoryFragment.kt
+++ app/src/main/java/no/hiof/toyopoly/CategoryFragment.kt
@@ -1,31 +1,71 @@
 package no.hiof.toyopoly
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
 
 
 class CategoryFragment : Fragment() {
     private val args: CategoryFragmentArgs by navArgs()
+    private lateinit var adsArrayList: ArrayList<Ads>
+    private lateinit var adpaterAds: AdpaterAds
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false)
+        val view = inflater.inflate(R.layout.fragment_category, container, false)
+
+        view.recyclerView.layoutManager = LinearLayoutManager(activity)
+        view.recyclerView.adapter = adapterAds()
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val categoryName = view.findViewById<TextView>(R.id.categoryClicked)
-        categoryName.text = args.category
+        getAds()
     }
-}
+
+    fun getAds(){
+        db = FirebaseFirestore.getInstance()
+        db.collection("ads")
+            .whereEqualTo("Category", "Toy-car")
+            .addSnapshotListener(object : EventListener<QuerySnapshot>{
+                override fun onEvent(value: QuerySnapshot?, error: FirebaseFirestoreException?) {
+                    if(error != null){
+                        Log.e("Firestore ERROR", error.message.toString())
+                        return
+                    }
+                    for ( dc : DocumentChange in value?.documentChanges!!) {
+                        if (dc.type == DocumentChange.Type.ADDED){
+                            adsArrayList.add(dc.document.toObject(Ads::class.java))
+                        }
+                    }
+                    adpaterAds.notifyDataSetChanged()
+                }
+
+            })
+    }
+
+    }
