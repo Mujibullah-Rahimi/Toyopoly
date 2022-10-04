c9eef6c90f0687141920918be725227c3704f135
MujibullahRahimi <mujibulr@hiof.no>
2022-10-04 08:23:13 +0200

    fix: deleted unnecessary file

--- app/src/main/java/no/hiof/toyopoly/Ads.kt
+++
@@ -1,5 +0,0 @@
-package no.hiof.toyopoly
-
-import java.security.Timestamp
-
-data class Ads(var value: String = "", var description: String = "", var price: String = "", var category: String = "", var Id: String ="")
