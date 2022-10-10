2078bde319a5cf16975f9efe1d59d2b767edf094
MujibullahRahimi <mujibulr@hiof.no>
2022-10-10 14:47:10 +0200

    fix: updated gradle settings|build /updated strings / removed unnecessary snippets

--- app/src/main/java/no/hiof/toyopoly/adapter/ChatListAdapter.kt
+++ app/src/main/java/no/hiof/toyopoly/adapter/ChatListAdapter.kt
@@ -5,41 +5,32 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.firestore.*
 import no.hiof.toyopoly.R
 import no.hiof.toyopoly.model.MessageModel
 
 
-class ChatListAdapter(private val messageList: ArrayList<MessageModel>) : RecyclerView.Adapter<ChatListAdapter.ChatViewHolder>() {
-    private lateinit var db : FirebaseFirestore
-    private lateinit var chatListAdapter: ChatListAdapter
+class ChatListAdapter(
+    private val messageList: ArrayList<MessageModel>,
+    private val listener : (MessageModel) -> Unit)
+    : RecyclerView.Adapter<ChatListAdapter.ChatViewHolder>() {
 
-
-    class ChatViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        // Gets a reference to all the specific views we are going to use or fill with data
+    inner class ChatViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val message : TextView = view.findViewById(R.id.chatRecyclerView)
-        fun bind(item: MessageModel){
-            // Fills the views with the given data
+
+        fun bind(item: MessageModel) = with(itemView){
             message.text = item.message
-
-
+            setOnClickListener{listener(item)}
         }
     }
-
 
     // Called when there's a need for a new ViewHolder (a new item in the list/grid)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder {
         val messageView = LayoutInflater.from(parent.context).inflate(R.layout.fragment_message_item,parent,false)
-        // Create the viewHolder with the corresponding view (list item)
         return ChatViewHolder(messageView)
     }
 
-
-    // Called when data is bound to a specific ViewHolder (and item in the list/grid)
     override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
-        // Gets the movie data we are going to use at the given position
-        val currentMessage : MessageModel =  messageList[position]
-        holder.bind(currentMessage)
+        holder.bind(messageList[position])
     }
 
     override fun getItemCount(): Int {
