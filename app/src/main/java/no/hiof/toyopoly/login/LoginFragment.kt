a834a5cefb3d0e0f9b4d12c6954d86f9d062b652
MujibullahRahimi <mujibulr@hiof.no>
2022-10-11 17:17:56 +0200

    fix: moved google-button outside of drawer-layout

--- app/src/main/java/no/hiof/toyopoly/login/LoginFragment.kt
+++ app/src/main/java/no/hiof/toyopoly/login/LoginFragment.kt
@@ -80,8 +80,8 @@ class LoginFragment : Fragment(), View.OnClickListener {
     override fun onResume() {
         super.onResume()
         (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
+        
     }
-
     override fun onStop() {
         super.onStop()
         (activity as AppCompatActivity?)!!.supportActionBar!!.show()
