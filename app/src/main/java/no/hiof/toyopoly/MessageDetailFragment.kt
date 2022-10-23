fc35457288f7fff54fd33f7fb8d52fd0de6ac943
MujibullahRahimi <mujibulr@hiof.no>
2022-10-23 16:06:29 +0200

    feat: messageDetailFragment where users chat together

---
+++ app/src/main/java/no/hiof/toyopoly/MessageDetailFragment.kt
@@ -0,0 +1,87 @@
+package no.hiof.toyopoly
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.firebase.Timestamp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.*
+import no.hiof.toyopoly.model.MessageModel
+import no.hiof.toyopoly.util.RandomId
+
+
+class MessageDetailFragment : Fragment(), View.OnClickListener  {
+    private lateinit var messageList: ArrayList<MessageModel>
+    private lateinit var auth: FirebaseAuth
+    private val db = FirebaseFirestore.getInstance()
+
+    val TAG = "MESSAGE"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_message, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+
+        val sendButton = view.findViewById<Button>(R.id.sendMessageButton)
+        sendButton?.setOnClickListener(this)
+
+    }
+
+    fun saveMsg(){
+        val messageInput = view?.findViewById<EditText>(R.id.editText_message)
+        val sendMsgButton = view?.findViewById<Button>(R.id.sendMessageButton)
+        val message = messageInput?.text.toString()
+
+        val messageToSave = MessageModel(RandomId.randomID(),message, Timestamp.now(), auth.currentUser!!.uid)
+
+        if (messageToSave.message.isNotEmpty()){
+            db.collection("Messages").document()
+                .set(messageToSave)
+                .addOnCompleteListener { Log.v(TAG, "Messaged saved")
+                    messageInput!!.text.clear()}
+                .addOnFailureListener{
+                    Toast.makeText(activity, it.message, Toast.LENGTH_LONG)
+                        .show()
+                }
+        }
+    }
+    fun getMessages(){
+        db.collection("Messages")
+            .addSnapshotListener(object : EventListener<QuerySnapshot> {
+                override fun onEvent(value: QuerySnapshot?, error: FirebaseFirestoreException?) {
+                    if(error != null){
+                        Log.e("Firestore ERROR", error.message.toString())
+                        return
+                    }
+                    for ( dc : DocumentChange in value?.documentChanges!!) {
+                        if (dc.type == DocumentChange.Type.ADDED){
+                            messageList.add(dc.document.toObject(MessageModel::class.java))
+                        }
+                    }
+                    //ChatListAdapter.notifyDataSetChanged()
+                }
+            })
+    }
+
+    override fun onClick(view: View?) {
+        saveMsg()
+    }
+}
