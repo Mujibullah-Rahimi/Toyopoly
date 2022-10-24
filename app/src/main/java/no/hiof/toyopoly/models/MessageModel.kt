d12e70ad5f1bfe06ee7028b5679d77b0f6fdade7
MujibullahRahimi <mujibulr@hiof.no>
2022-10-24 22:02:59 +0200

    feat: added constructor to messageModel

--- app/src/main/java/no/hiof/toyopoly/models/MessageModel.kt
+++ app/src/main/java/no/hiof/toyopoly/models/MessageModel.kt
@@ -1,9 +1,13 @@
 package no.hiof.toyopoly.models
 
+import com.google.firebase.Timestamp
+
 data class MessageModel(
     val messageId : String,
     val message : String,
     val timestamp: com.google.firebase.Timestamp,
     val senderId : String,
     //val receiverId : String
-    ){}
+    ){
+    constructor() :this(messageId = "", message = "", timestamp = Timestamp.now(), senderId = "")
+}
