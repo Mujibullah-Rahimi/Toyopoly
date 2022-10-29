830651692175228167af1b0b63ade4ead068a8ef
MujibullahRahimi <mujibulr@hiof.no>
2022-10-29 17:31:13 +0200

    feat: CreateAd fragment now creates ads with images saved in storage, with uri saved in firestore

--- app/src/main/java/no/hiof/toyopoly/ad/CreateAdFragment.kt
+++ app/src/main/java/no/hiof/toyopoly/ad/CreateAdFragment.kt
@@ -1,9 +1,6 @@
 package no.hiof.toyopoly.ad
 
-import android.Manifest
-import android.content.ContentValues.TAG
-import android.content.Intent
-import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.app.Activity
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -11,33 +8,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.github.dhaval2404.imagepicker.ImagePicker
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
-import com.wajahatkarim3.easyvalidation.core.view_ktx.*
+import com.wajahatkarim3.easyvalidation.core.view_ktx.maxLength
+import com.wajahatkarim3.easyvalidation.core.view_ktx.minLength
+import com.wajahatkarim3.easyvalidation.core.view_ktx.nonEmpty
+import com.wajahatkarim3.easyvalidation.core.view_ktx.onlyNumbers
 import no.hiof.toyopoly.R
 import no.hiof.toyopoly.models.AdModel
-import no.hiof.toyopoly.models.PhotoModel
 import no.hiof.toyopoly.util.RandomId
+import java.util.*
 
 class CreateAdFragment : Fragment() {
-    private var uri: Uri? = null
 
-    //private lateinit var currentImagePath: String
-    val db = Firebase.firestore
-    val user = Firebase.auth.currentUser
-    private var photos: ArrayList<PhotoModel> = ArrayList<PhotoModel>()
-    private var storageReference = FirebaseStorage.getInstance().getReference()
-    private lateinit var photo: PhotoModel
+    // objects and lists
+    private var adImages: ArrayList<ImageView> = ArrayList<ImageView>()
+    private val user = Firebase.auth.currentUser
+    private lateinit var ad: AdModel
 
-    //private lateinit var viewModel : AdsViewModel
-    private lateinit var ads: AdModel
-    //private lateinit var _binding :
+    // database instances and references
+    private val db = Firebase.firestore
+    private var storageRef = FirebaseStorage.getInstance().reference
+
+    // strings
+    private var TAG = "PHOTO"
+    private lateinit var adImage: ImageView
+    private var imageURI: Uri? = null
+
+    //private lateInit var viewModel : AdsViewModel
+    //private lateInit var _binding :
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -50,6 +57,17 @@ class CreateAdFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //viewModel = ViewModelProvider.get(AdsViewModel::class.java)
+        adImage = view.findViewById(R.id.adImageView)
+        val galleryBtn = view.findViewById<Button>(R.id.galleryBtn)
+        galleryBtn.setOnClickListener {
+            //takePhoto()
+            ImagePicker.with(requireActivity())
+                .galleryOnly()
+                .maxResultSize(1080, 1080)  //Final image resolution will be less than 1080 x 1080(Optional)
+                .createIntent { intent ->
+                    getImageResult.launch(intent)
+                }
+        }
 
         val spinner: Spinner = view.findViewById(R.id.spinner_catergory)
         this.activity?.let {
@@ -62,151 +80,49 @@ class CreateAdFragment : Fragment() {
                 spinner.adapter = adapter
             }
         }
+
         val saveButton = view.findViewById<Button>(R.id.createAd)
         saveButton.setOnClickListener {
             saveAd(RandomId.randomID())
         }
-
-        val photoBtn = view.findViewById<Button>(R.id.photoBtn)
-        photoBtn.setOnClickListener {
-            takePhoto()
-        }
     }
 
-    private fun takePhoto() {
-        if (hasCameraPermission() == PERMISSION_GRANTED && hasExternalWriteStoragePermission() == PERMISSION_GRANTED && hasExternalReadStoragePermission() == PERMISSION_GRANTED) {
-            //invokeCamera()
-            openGallery()
+    private val getImageResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result: ActivityResult ->
+        val resultCode = result.resultCode
+        val data = result.data
+
+        if (resultCode == Activity.RESULT_OK) {
+            //Image Uri will not be null for RESULT_OK
+            val fileUri = data?.data!!
+            imageURI = fileUri
+            Log.v(TAG, "$fileUri")
+            adImage.setImageURI(fileUri)
+
+        } else if (resultCode == ImagePicker.RESULT_ERROR) {
+            Toast.makeText(activity, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+            Log.v(TAG, ImagePicker.getError(data))
         } else {
-            requestMultiplePermissionsLauncher.launch(
-                arrayOf(
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                    Manifest.permission.CAMERA,
-                    Manifest.permission.READ_EXTERNAL_STORAGE
-                )
-            )
+            Toast.makeText(activity, "Task Cancelled", Toast.LENGTH_SHORT).show()
+            Log.v(TAG, ImagePicker.getError(data))
         }
     }
-
-    private val requestMultiplePermissionsLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { resultsMap ->
-            var permissionGranted = false
-            resultsMap.forEach {
-                if (it.value == true) {
-                    permissionGranted = it.value
-                } else {
-                    permissionGranted = false
-                    return@forEach
-                }
-            }
-            if (permissionGranted) {
-                //invokeCamera()
-                openGallery()
-            } else {
-                Toast.makeText(this.activity, "No camera without permission", Toast.LENGTH_LONG)
-                    .show()
-            }
-        }
-
-
-    private fun openGallery() {
-        val intent : Intent = Intent().apply {
-            type = "image/*"
-            action = Intent.ACTION_PICK
-            putExtra(Intent.EXTRA_STREAM, uri)
-        }
-
-        getResult.launch((Intent.createChooser(intent, null)))
-    }
-
-    private val getResult =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            if (uri != null) {
-                Log.d(TAG, "Her var det en URI ja")
-                photo.localUri = it.data?.data!!.toString()
-                getCameraImage.launch(uri)
-            }else{
-                Log.e(TAG, "Wrong with the URI")
-            }
-        }
-    private val getCameraImage = registerForActivityResult(ActivityResultContracts.TakePicture()){
-        success ->
-      if(success){
-          Log.i(TAG, "Img loc: ${uri}")
-      }else{
-         Log.e(TAG,"img no bueno. ${uri}")
-     }
-   }
-
-    private fun uploadImg() {
-        var uri = Uri.parse(photo.localUri)
-        var imageRef = storageReference.child("images/${user?.uid}/${uri.lastPathSegment}")
-        val uploadTask = imageRef.putFile(uri)
-        uploadTask.addOnSuccessListener {
-            Log.i(TAG, "Image uploaded $imageRef")
-            val downloadUrl = imageRef.downloadUrl
-            downloadUrl.addOnSuccessListener { remoteUri ->
-                photo.remoteUri = remoteUri.toString()
-                updatePhotoDatabase(photo)
-            }
-        }
-        uploadTask.addOnFailureListener {
-            Log.e(TAG, it.message ?: "no message")
-        }
-    }
-
-    private fun updatePhotoDatabase(photo: PhotoModel) {
-        var photoCollection = db.collection("Ads").document(ads.adId).collection("Images")
-        var handle = photoCollection.add(photo)
-        handle.addOnSuccessListener {
-            Log.i(TAG, "Successfully updated photo metadata")
-            photo.id = it.id
-            db.collection("Ads").document(ads.adId).collection("Images").document(photo.id)
-                .set(photo)
-        }
-        handle.addOnFailureListener {
-            Log.e(TAG, "Error updating photo data: ${it.message}")
-        }
-    }
-
-    fun hasCameraPermission() =
-        this.activity?.let { ContextCompat.checkSelfPermission(it, Manifest.permission.CAMERA) }
-
-    fun hasExternalWriteStoragePermission() = this.activity?.let {
-        ContextCompat.checkSelfPermission(
-            it,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-        )
-    }
-
-    fun hasExternalReadStoragePermission() = this.activity?.let {
-        ContextCompat.checkSelfPermission(
-            it,
-            Manifest.permission.READ_EXTERNAL_STORAGE
-        )
-    }
-
 
     fun saveAd(documentId: String) {
         val title = view?.findViewById<EditText>(R.id.title_create_ad)
-        val titleFire = title?.text.toString()
         val desc = view?.findViewById<EditText>(R.id.desc_createAd)
-        val descFire = desc?.text.toString()
         val prices = view?.findViewById<EditText>(R.id.price_createAd)
-        val priceFire = prices?.text.toString()
         val spinner = view?.findViewById<Spinner>(R.id.spinner_catergory)
-        val spinnerFire = spinner?.selectedItem.toString()
-        val userUID = user!!.uid
 
-        var adModel = AdModel().apply {
-            adId = documentId
-            value = title?.text.toString()
-            description = desc?.text.toString()
-            price = prices?.text.toString()
-            category = spinner?.selectedItem.toString()
-            remoteUri = photo!!.remoteUri
-            userId = user!!.uid
-        }
+        val adToSave = AdModel(
+            adId = documentId,
+            title = title?.text.toString().replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.ROOT) else it.toString() },
+            description = desc?.text.toString(),
+            price = prices?.text.toString(),
+            category = spinner?.selectedItem.toString(),
+            imageUri = "",
+            userId = user!!.uid,
+            timestamp = Timestamp.now()
+        )
 
         if (
             !title!!.nonEmpty() ||
@@ -218,27 +134,27 @@ class CreateAdFragment : Fragment() {
                 "All fields needs to be filled",
                 Toast.LENGTH_LONG
             ).show()
-        } else if (!title.maxLength(16) || !title.atleastOneUpperCase()) {
+        } else if (!title.maxLength(20)) {
             Toast.makeText(
                 activity,
-                "The title can only be 10 chars long and must have at least one uppercase char",
+                "Title is too long",
                 Toast.LENGTH_LONG
             ).show()
         } else if (!desc.minLength(16)) {
             Toast.makeText(
                 activity,
-                "The description must at least be 16 chars long",
+                "Description is too short",
                 Toast.LENGTH_LONG
             ).show()
         } else if (!prices.onlyNumbers()) {
             Toast.makeText(
                 activity,
-                "The price can only consist of numbers",
+                "Price can only consist of digits",
                 Toast.LENGTH_LONG
             ).show()
         } else {
             db.collection("Ads").document(documentId)
-                .set(adModel)
+                .set(adToSave)
                 .addOnSuccessListener {
                     title.text.clear()
                     desc.text.clear()
@@ -249,11 +165,17 @@ class CreateAdFragment : Fragment() {
                 }.addOnCompleteListener {
                     val action =
                         CreateAdFragmentDirections.actionCreateAdsFragmentToAdDetailFragment(
-                            adModel.adId
+                            adToSave.adId
                         )
-                    val navController = view?.findNavController()
-                    navController?.navigate(action)
-                    uploadImg()
+
+                    storageRef.child("images/ads/${adToSave.adId}").putFile(imageURI!!)
+                        .addOnSuccessListener {
+                            db.collection("Ads").document(adToSave.adId).update("imageUri", it.storage.path)
+
+                            val navController = view?.findNavController()
+                            navController?.navigate(action)
+                        }
+
                 }
                 .addOnFailureListener {
                     Toast.makeText(activity, it.message, Toast.LENGTH_LONG)
@@ -262,5 +184,3 @@ class CreateAdFragment : Fragment() {
         }
     }
 }
-
-
